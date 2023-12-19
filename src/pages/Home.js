a2edc662d84ac6ee9be8ee26c3f3037451dcb677
meanzar <116259100+Meanzar@users.noreply.github.com>
2023-12-19 15:49:09 +0100

    Feature/add auth (#2)
    * add login
    
    * add new field & rename files

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
+
+export default function HomePage() {
+  return (
+    <Authenticator signUpAttributes={[
+      'email',
+      'name',
+      'family_name',
+    ]}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
