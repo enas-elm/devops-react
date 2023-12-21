5d9d0dfebe43e201dbb160a8328f0437767693f5
Meanzar <romain.danizel.pro@gmail.com>
2023-12-21 14:49:06 +0100

    add api env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,30 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apib3b4037a": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "react256ed71b7": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
   },
   "function": {
+    "react227b13b0e": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "react256ed71b7PostConfirmation": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
